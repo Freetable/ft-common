7197874b5776c0cd0b64f93b60f61252b89bc232
Stephanie Sunshine <stephanie@nirvanadesktop.com>
2014-01-13 12:45:25 -0800

    updating class methods

--- lib/ft-common.rb
+++ lib/ft-common.rb
@@ -4,7 +4,7 @@ class Freetable
 	RETURNFAIL    = '{"result": "0"}'
 	RETURNSUCCESS = '{"result": "1"}'
 
-	def validate( values, types )
+	def self.validate( values, types )
 	  fail = false
   	bake = []
   	(0..values.length).each do |c|
