5b025d5e55de609790e70826e442ff391e859f3a
Stephanie Sunshine <stephanie@nirvanadesktop.com>
2014-01-13 14:38:48 -0800

    Adding more case types

--- ft-common.gemspec
+++ ft-common.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'ft-common'
-  s.version     = '0.0.8'
+  s.version     = '0.0.10'
   s.date        = '2014-01-13'
   s.summary     = "A common Ruby class between the applications."
   s.description = "A common Ruby class between the applications."
--- lib/ft-common.rb
+++ lib/ft-common.rb
@@ -17,12 +17,20 @@ class Freetable
         	fail = true if val !~ /^[\h]{8}-[\h]{4}-[\h]{4}-[\h]{4}-[\h]{12}$/
       	when 'integer'
         	fail = true if val.to_i != val
-      	when 'password'
-        	fail = true if val !~ /^[0-9a-f]{128}$/
 				when 'username'
 					fail = true if val =~ /\'|\"/
 				when 'email'
 					fail = true if val !~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+        when 'year'
+          fail = true if val !~ /^[\d]{4}$/
+        when 'md5'
+          fail = true if val !~ /^[\h]{32}$/
+        when 'sha1'
+          fail = true if val !~ /^[\h]{160}$/
+        when 'sha512'
+          fail = true if val !~ /^[\h]{128}$/
+        when 'skip'
+          1
     end
   end
 	return !fail
